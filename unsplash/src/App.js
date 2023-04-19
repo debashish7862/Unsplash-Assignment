10667a3e5c40385f2247e9af68ecee93b423a814
debashish sahu <debashish7862@gmail.com>
2023-04-19 22:03:27 +0530

    yes

---
+++ unsplash/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Component/Nav';
+import Main from './Component/Main';
+
+function App() {
+  return (
+    <div>
+   <Nav/>
+   <Main/>
+    </div>
+  );
+}
+
+export default App;
